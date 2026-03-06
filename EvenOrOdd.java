daf1f12e338b7f3516d78c0e96d0285621f7ffd8
Anishk-hub <183097098+Anishk-hub@users.noreply.github.com>
2026-03-06 12:24:42 +0530

    Java program VoteEligibility

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -1,6 +1,6 @@
 public class EvenOrOdd {
     public static void main(String[] args) {
-        int num = 37;
+        int num = 50;
         if(num%2==0){
             System.out.println("Number is even:"+ num);
         }else{
---
+++ VoteEligibility.java
@@ -0,0 +1,10 @@
+public class VoteEligibility {
+    public static void main(String[] args) {
+        int age = 23;
+        if(age>=18){
+            System.out.println("Person can vote");
+        }else{
+            System.out.println("Person cannot vote");
+        }
+    }
+}
